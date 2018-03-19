7482d7621cedf1e387aa264bf852da591d6173b7
jms <jms@218.38.137.27>
2018-03-19 10:45:27 +0900

    쿠폰 발행 키 생성할 때 랜덤 보수를 추가시켜 알파벳 범위를 전체적으로 사용 할 수 있도록 함.

--- src/main/java/com/coupon/issue/util/CouponGenerator.java
+++ src/main/java/com/coupon/issue/util/CouponGenerator.java
@@ -1,5 +1,7 @@
 package com.coupon.issue.util;
 
+import java.security.SecureRandom;
+import java.util.Random;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -14,7 +16,8 @@ public class CouponGenerator {
   public String getGeneratorCoupon() {
     sb = new StringBuilder();
     String[] strArray = getNumbers().split("");
-    //LOG.info(Arrays.toString(strArray));
+    Random ran = new SecureRandom();
+    int ranNumber = ran.nextInt(16);
     for (int i = 0; i < 16; i++) {
       int number = Integer.parseInt(strArray[i]);
       if (i > 0 && i % 4 == 0) {
@@ -23,13 +26,13 @@ public class CouponGenerator {
       int mod = number % 3;
       switch (mod) {
         case 0:
-          sb.append((char) (97 + number));
+          sb.append((char) (97 + number + ranNumber));
           break;
         case 1:
-          sb.append(number);
+          sb.append(number + ranNumber >= 10 ? number : number + ranNumber);
           break;
         case 2:
-          sb.append((char) (65 + number));
+          sb.append((char) (65 + number + ranNumber));
           break;
         default:
           break;
